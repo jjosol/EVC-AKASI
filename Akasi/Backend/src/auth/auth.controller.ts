b9fda2b46999cf2b503b67e2642c3312f63ae4f1
neuve-n <145216669+nueve-n@users.noreply.github.com>
2025-03-26 18:25:05 +0800

    w/ manager files

--- Akasi/Backend/src/auth/auth.controller.ts
+++ Akasi/Backend/src/auth/auth.controller.ts
@@ -6,7 +6,7 @@ import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post('login')
   @HttpCode(HttpStatus.OK)
